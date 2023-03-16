f0bc7ec0d53c730e535adc88baca6440a0fb01f8
cynthia41 <cynthiaadhiambo41@gmail.com>
2023-03-16 17:33:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
 
-// Write your code here
+let random = 2;
+Math.random()*10 + 2;
+
+let num3 = 8;
+let num4 = 2;
+let mod = num3 / num4;
+
+let max = Math.max(12, 14, 15, 17, 20);
+ 
